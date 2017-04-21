b4aa7ce2593bac91634d1a27e73a442e8c0d3044
Adam Gray <adam@pateo.co>
2017-04-21 15:08:37 +0100

    Refactor createMap

--- map.js
+++ map.js
@@ -26,67 +26,43 @@ function Map() {
   }
 
   this.createMap = (node) => {
-    let top = ''
-    let center = ''
-    let bottom = ''
+    let top = center = bottom = ''
     let spacing = Array(20).join(' ')
     let divider = '----'
 
-    if(node.getWest() != null) {
-      center += node.getWest().name
-      center += spacing
-    }else {
-      center += divider
-      center += spacing
-    }
-
-    center += '*' + node.name + '*'
-
-    if(node.getEast() != null) {
-      center += spacing
-      center += node.getEast().name
-    }else {
-      center += spacing
-      center += divider
-    }
+    ;(node.getWest() != null) ? center += node.getWest().name : center += divider
+    center += `${spacing}*${node.name}*${spacing}`
+    ;(node.getEast() != null) ? center += node.getEast().name : center += divider
 
     if(node.getNorth() != null) {
-      if(node.getWest() != null) {
-        top += (Array(node.getWest().name.length + 1).join(' ') + spacing)
-      }else {
-        top += (Array(divider.length + 1).join(' ') + spacing)
-      }
+      top += combineSpace()
       top += node.getNorth().name
     }else {
-      if(node.getWest() != null) {
-        top += (Array(node.getWest().name.length + 1).join(' ') + spacing)
-      }else {
-        top += (Array(divider.length + 1).join(' ') + spacing)
-      }
+      top += combineSpace(top)
       top += divider
     }
 
     if(node.getSouth() != null) {
-      if(node.getWest() != null) {
-        bottom += (Array(node.getWest().name.length + 1).join(' ') + spacing)
-      }else {
-        bottom += (Array(divider.length + 1).join(' ') + spacing)
-      }
+      bottom += combineSpace(bottom)
       bottom += node.getSouth().name
     }else {
-      if(node.getWest() != null) {
-        bottom += (Array(node.getWest().name.length + 1).join(' ') + spacing)
-      }else {
-        bottom += (Array(divider.length + 1).join(' ') + spacing)
-      }
+      bottom += combineSpace(bottom)
       bottom += divider
     }
 
-    console.log('')
-    console.log(top)
-    console.log(center)
-    console.log(bottom)
-    console.log('')
+    function combineSpace() {
+      if(node.getWest() != null) {
+        return (Array(node.getWest().name.length + 1).join(' ') + spacing)
+      }else {
+        return (Array(divider.length + 1).join(' ') + spacing)
+      }
+    }
+
+    console.log(`
+    ${top}
+    ${center}
+    ${bottom}
+    `)
 
   }
 
