bb0a43ae52e7e71a652adc44758102091f4d3dbf
Adam Gray <adam@pateo.co>
2017-04-26 18:59:04 +0100

    Add a couple of npc characters

--- src/npc.js
+++ src/npc.js
@@ -1,6 +1,24 @@
 class NPC {
+  constructor() {
+    this.characters = [
+      {
+        name: 'Krumuahan Blackdust',
+        type: 'Dwarf'
+      },
+      {
+        name: 'Shaaedan Bladeshadow',
+        type: 'Night elf'
+      }
+    ]
+  }
+
   event() {
-    console.log('You are greeted by an NPC...')
+    let character = this.getCharacter()
+    console.log(`You are greeted by ${character.name} the ${character.type}!`)
+  }
+
+  getCharacter() {
+    return this.characters[Math.floor(Math.random() * this.characters.length)]
   }
 }
 
