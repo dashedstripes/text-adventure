df27353d80742f4cf0d0edf484f6fe927725852f
Adam Gray <adam@pateo.co>
2017-04-26 18:41:44 +0100

    Player health should not reach above 100

--- src/item.js
+++ src/item.js
@@ -8,8 +8,20 @@ class Item {
     if(player != null) {
       if(player.health < 100) {
         if(this.options.health != null) {
-          player.health += this.options.health
-          console.log(`You used ${this.name} and gained ${this.options.health} health!`)
+          let itemHealth = this.options.health
+          let healthBefore = player.health
+          let healthAfter = player.health + this.options.health
+          let healthRecovered = 0
+
+          if(healthAfter > 100) {
+            let healthToAdd = itemHealth + (100 - healthAfter)
+            healthRecovered = healthToAdd
+            player.health += healthToAdd
+          }else {
+            healthRecovered = this.options.health
+            player.health += this.options.health
+          }
+          console.log(`You used ${this.name} and recovered ${player.health - healthBefore} health!`)
           return true
         }
       }else {
